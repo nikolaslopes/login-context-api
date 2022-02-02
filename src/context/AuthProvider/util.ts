8c99476d97cfd86d8790d88d41798701ed88a82f
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-02 12:24:09 -0400

    :alien: prepare local storage

--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,4 +1,21 @@
 import { Api } from '../../services/api';
+import { IUser } from './types';
+
+export function setUserLocalStorage(user: IUser | null) {
+  localStorage.setItem("key", JSON.stringify(user));
+}
+
+export function getUserLocalStorage() {
+  const json = localStorage.getItem("key");
+
+  if (!json) {
+    return null;
+  }
+
+  const user = JSON.parse(json);
+
+  return user;
+}
 
 export async function LoginRequest(email: string, password: string) {
   try {
