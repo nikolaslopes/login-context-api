49bb02d31647208d4e6b8b4b620c2ece2098b9fe
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-02 11:29:07 -0400

    :fire: provider already

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -5,4 +5,18 @@ export const AuthContext = createContext<IContext>({} as IContext);
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
   const [user, setUser] = useState<IUser | null>();
+
+  async function authenticate(email: string, password: String) {
+
+  }
+
+  function logout() {
+
+  }
+
+  return (
+    <AuthContext.Provider value={{...user, authenticate, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
