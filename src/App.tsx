7317f8c0b53f1f08b16af56f9c9de45b8fd7df7c
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-02 16:33:09 -0400

    :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProtectedLayout } from './components/ProtectedLayout';
 import { AuthProvider } from './context/AuthProvider';
 
 
 function App() {
-
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/profile" />
+          <Route path="/profile"
+            element={
+              <ProtectedLayout>
+                <h1>You are authenticated</h1>
+              </ProtectedLayout>
+            }
+          />
 
-          <Route path="/login"/>
+          <Route path="/login" element={<h1>aaa</h1>}/>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
   )
 }
-
 export default App;
--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -1,9 +1,11 @@
 import { useAuth } from '../../context/AuthProvider/useAuth';
 
-export const ProtectedLayout = () => {
+export const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
   const auth = useAuth();
 
   if (!auth.email) {
-    return <h1>You don't have access</h1>
+    return <h1>You don't have access</h1>;
   }
+
+  return children;
 };
