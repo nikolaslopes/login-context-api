cff7d72d28be5ac2ba0ea302f797f6e44d0b848c
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-03 09:44:46 -0400

    :fire:

--- src/Pages/Login/styles.ts
+++ src/Pages/Login/styles.ts
@@ -5,12 +5,18 @@ export const Container = styled.div`
   border-radius: 8px;
   box-shadow: 0px 24px 38px #00000024;
 
-  width: 800px;
+  max-width: 722px;
+  width: 100%;
   height: 600px;
+  margin: 0 auto;
 
   padding: 20px;
-
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 720px) {
+    width: 400px;
+    height: 400px;
+  }
 `;
