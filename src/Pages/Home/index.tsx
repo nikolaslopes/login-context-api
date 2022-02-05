edc244a233eed4eee765105e827502e2331f13e4
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-04 23:40:53 -0400

    :fire: home created

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <h1>aaa</h1>
+    </Container>
+  );
+}
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,11 +1,14 @@
 import { Routes as RoutesWrapper, Route } from 'react-router-dom';
 
 import { ProtectedLayout } from '../components/ProtectedLayout';
+import { Home } from '../Pages/Home';
 import { Login } from '../Pages/Login';
 
 export function Routes() {
   return (
     <RoutesWrapper>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route
         path="/profile"
         element={
@@ -14,7 +17,6 @@ export function Routes() {
           </ProtectedLayout>
         }
       />
-      <Route path="/login" element={<Login />} />
     </RoutesWrapper>
   );
 }
