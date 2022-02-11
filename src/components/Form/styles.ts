f17e79402a01eb227964331ed661aa4336618f3e
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-10 23:41:09 -0400

    :dizzy: animation to input

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,6 +1,27 @@
+import { motion, Variants } from 'framer-motion';
 import styled from 'styled-components';
 
-export const Container = styled.form`
+const containerVariants: Variants = {
+  hidden: {
+    x: -400,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      duration: 1.2,
+      type: 'spring',
+      bounce: 0.5,
+    },
+  },
+};
+export const Container = styled(motion.form).attrs(() => ({
+  variants: containerVariants,
+  initial: 'hidden',
+  animate: 'visible',
+}))`
   padding: 142px;
   display: flex;
   flex-direction: column;
