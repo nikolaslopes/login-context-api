114e91b45be01263c4833baf61b41b7dece493d7
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-03-17 22:08:04 -0400

    :fire:

--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -4,7 +4,7 @@ export function ProtectedLayout({ children }: { children: JSX.Element }) {
   const auth = useAuth();
 
   if (!auth.email) {
-    return <h1>You don't have access</h1>;
+    return <h1>You do not have access</h1>;
   }
 
   return children;
